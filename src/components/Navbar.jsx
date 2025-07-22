b34d739f3cae09b886e30612685ff85fe26b6eeb
roniadiatmoko <arsenalroni@gmail.com>
2025-07-22 23:03:04 +0700

    fix: change title bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,23 +17,23 @@ function Navbar() {
     <header className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${scrolled? 'shadow-md bg-white/70 backdrop-blur-lg' : ''}`}>
       <div className="container mx-auto px-4 md:px-8 py-4 flex justify-between items-center">
         <div className="flex items-center">
-          <span className="text-blue-300 font-bold text-2xl">SIAKAD</span>
+          <span className="text-blue-300 font-bold text-2xl">UNIVERSITY</span>
         </div> 
         
         <nav>
           <ul className="flex space-x-6"> 
             <li>
-              <a href="#hero" className="text-white font-medium hover:hover:font-medium transition duration-300">
+              <a href="#hero" className="text-black font-medium hover:hover:font-medium transition duration-300">
                 Beranda
               </a>
             </li>
             <li>
-              <a href="#programs" className="text-white hover:hover:font-medium font-thin transition duration-300">
+              <a href="#programs" className="text-black hover:hover:font-medium font-thin transition duration-300">
                 Program Studi
               </a>
             </li>
             <li>
-              <a href="#login" className="text-white hover:hover:font-medium font-thin transition duration-300">
+              <a href="#login" className="text-black hover:hover:font-medium font-thin transition duration-300">
                 Login
               </a>
             </li>
